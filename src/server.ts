9bea8b8db6c3aef9e695f7016c79a44eb8184951
Evelyn Helena <evelyn.helena@ifood.com.br>
2025-06-22 15:16:39 -0300

    teste corss

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import 'dotenv/config';
+
 import Fastify from 'fastify';
+import cors from '@fastify/cors'; // 👈 import do plugin de CORS
 import userRoutes from './routes/userRoutes';
 
 const PORT = parseInt(process.env.PORT || '3000', 10);
 const app = Fastify({ logger: true });
 
+// 👇 Registra o CORS permitindo a origem do seu frontend Vercel
+app.register(cors, {
+  origin: 'https://lumi-bank-khlb0sh08-evelynhelena1gmailcoms-projects.vercel.app',
+  credentials: true, // Se você precisar enviar cookies/autenticação
+});
+
+// Registra rotas depois do CORS
 app.register(userRoutes);
 
 app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
